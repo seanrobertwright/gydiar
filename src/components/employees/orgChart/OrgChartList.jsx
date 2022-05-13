51a5e142c02092494c5c227196b68ad2c7afa39c
seanrobertwright <seanrobertwright@gmail.com>
2022-05-13 10:13:33 -0400

    tree clean

---
+++ src/components/employees/orgChart/OrgChartList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const OrgChartList = () => {
+  return (
+    <div>OrgChartList</div>
+  )
+}
+
+export default OrgChartList
--- src/components/employees/orgChart/index.js
+++ src/components/employees/orgChart/index.js
@@ -1,7 +1,9 @@
 import OrgChartIcon from '@mui/icons-material/People';
+import OrgChartList from './OrgChartList';
 import OrgChartShow from './OrgChartShow';
 
 export default {
+    list: OrgChartList,
     show: OrgChartShow,
     icon: OrgChartIcon,
 };
